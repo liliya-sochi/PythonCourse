b0738182f2c080fd8fc77cdb75301d754bfd9b56
liliya-sochi <71435863+liliya-sochi@users.noreply.github.com>
2022-08-07 10:10:17 +0300

    Update item_three.py

--- lesson_one/item_three.py
+++ lesson_one/item_three.py
@@ -3,7 +3,7 @@ step = 0
 temp = 0
 result = 0
 while step < n:
-        temp = temp * 10 + n
-        result += temp
-        step = step + 1
+    temp = temp * 10 + n
+    result += temp
+    step += 1
 print(result)
